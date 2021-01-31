c7fd9021afb5fd9a1bc8025bae22c9646602d5a2
daniel passy <danielpassy@hotmail.com>
2021-01-31 00:30:12 -0300

    redirected to missionSettings

--- django/backend/urls.py
+++ django/backend/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from . import views
 urlpatterns = [
-    path('', views.edit, name='edit'),
+    path('settings/', views.edit, name='edit'),
     path('login/', views.login, name='login'), 
     path('weekly/', views.weekly, name='weekly'),
     path('feedback/', views.feedback, name='feedback'),
--- django/backend/views.py
+++ django/backend/views.py
@@ -1,23 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth import authenticate, login as logUser
+from django.urls import reverse
 
 # Create your views here.
 def login(request):
     if request.method == "POST":
-        # create a form instance and populate it with data from the request:
-        # check whether it's valid:
         email = request.POST["email"]
         password = request.POST["psw"]
         user = authenticate(request, email=email, password=password)
         if user is not None:
             logUser(request, user)
-            return HttpResponseRedirect("/")
+            return HttpResponseRedirect(reverse('edit'))
         return render(
             request, "login.html", {"email": email, "password": password}
         )
-    else:
-        pass
     return render(request, "login.html")
 
 
@@ -26,8 +23,7 @@ def edit(request):
         pass
     else:
         pass
-    return HttpResponse("hi")
-    return render(request, "name.html", {"form": form})
+    return render(request, "missionSetting.html")
 
 
 def weekly(request):
