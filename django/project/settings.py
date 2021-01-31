ee10e22bfe3547981f314702c69a1b96e188e552
daniel passy <danielpassy@hotmail.com>
2021-01-30 23:51:27 -0300

    changed db settings

--- django/project/settings.py
+++ django/project/settings.py
@@ -76,11 +76,10 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(os.path.join(BASE_DIR, "db.sqlite3"))
     }
 }
 
